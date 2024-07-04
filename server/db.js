4f5acc24864f0f2e609bcb1e383f23c08715835a
Pirzada Abdur Rehman <138493046+pirzada03@users.noreply.github.com>
2024-07-04 21:47:12 +0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,6 +4,6 @@ const mongoose = require("mongoose");
 module.exports = () => {
 	const connection = mongoose
 		.connect(process.env.MONGODB_URI)
-		.then((result) => console.log("Connected to database"))
-		.catch((err) => console.log("could not connect to database"));
+		.then((result) => console.log("Connected to database. ",result))
+		.catch((err) => console.log("could not connect to database. Error: ",err));
 };
