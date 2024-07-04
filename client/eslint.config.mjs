439869be4eed7e59dcd5ee908318c760343d9499
Pirzada Abdur Rehman <138493046+pirzada03@users.noreply.github.com>
2024-07-04 22:12:10 +0500

    Updated eslint in client

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -4,17 +4,30 @@ import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } }},
-  {files: ["**/*.js"], languageOptions: {sourceType: "module"}},
-  {languageOptions: { globals: { ...globals.browser, ...globals.node } }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.recommended,
-  {plugins: { react: pluginReact, 'react-hooks': pluginReactHooks }},
-  {rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/no-unknown-property': ['error', { ignore: ['class', 'row', 'align'] }],
-    'react-hooks/exhaustive-deps': 'error',
-    'no-unused-vars': 'warn',
-  }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true }
+      },
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    plugins: {
+      js: pluginJs,
+      react: pluginReact,
+      'react-hooks': pluginReactHooks
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/no-unknown-property': ['error', { ignore: ['class', 'row', 'align'] }],
+      'react-hooks/exhaustive-deps': 'error',
+      'no-unused-vars': 'warn',
+    }
+  }
 ];
